c0020132dc734969591d5f02ad57966424ae0e02
Kirill Agalakov <agalakov@devexperts.com>
2016-07-21 14:49:11 +0300

    add clearer to memoize

--- src/function/memoize.js
+++ src/function/memoize.js
@@ -1,24 +1,32 @@
 /**
+ * @type {Symbol}
+ */
+export const MEMOIZE_CLEAR_FUNCTION = Symbol('MEMOIZE_CLEAR_FUNCTION');
+
+/**
  * Memoizes function for passed arguments
  * @param {Function} fn
  * @returns {Function}
  */
 export function memoize(fn) {
 	const storage = {};
-	return function() {
+	const result = function() {
 		const args = Array.prototype.slice.call(arguments);
-		const argsAreValid = args.every(arg => {
-			return typeof arg === 'number' || typeof arg === 'string';
-		});
-		if (!argsAreValid) {
-			throw Error('Arguments to memoized function can only be strings or numbers');
-		}
-		const key = JSON.stringify(args);
+		const key = serialize(args);
 		if (typeof storage[key] === 'undefined') {
 			storage[key] = fn.apply(this, args);
 		}
 		return storage[key];
 	}.bind(this);
+
+	//inject clearer
+	result[MEMOIZE_CLEAR_FUNCTION] = function() {
+		const args = Array.prototype.slice.call(arguments);
+		const key = serialize(args);
+		delete storage[key];
+	};
+
+	return result;
 }
 
 export default memoize;
@@ -43,4 +51,18 @@ export function MEMOIZE(target, property, descriptor) {
 		descriptor.value = memoize(descriptor.value);
 	}
 	return descriptor;
+}
+
+/**
+ * @param {Array.<*>} args
+ * @returns {String}
+ */
+function serialize(args) {
+	const argsAreValid = args.every(arg => {
+		return typeof arg === 'number' || typeof arg === 'string';
+	});
+	if (!argsAreValid) {
+		throw Error('Arguments to memoized function can only be strings or numbers');
+	}
+	return JSON.stringify(args);
 }
