1dd57b43cb9448b75656365ee1699984bcd65ed4
Kirill Agalakov <agalakov@devexperts.com>
2016-12-06 11:22:10 +0300

    uuid

--- src/string/string.js
+++ src/string/string.js
@@ -100,3 +100,13 @@ export function pluralize2(number, declensions) {
 		return declensions[1];
 	}
 }
+
+/**
+ * Generates universal unique id
+ * @see https://gist.github.com/gordonbrander/2230317#gistcomment-1618310
+ * @returns {String}
+ */
+export function uuid() {
+	const chr4 = () => Math.random().toString(16).slice(-4);
+	return `${chr4() + chr4()}-${chr4()}-${chr4()}-${chr4()}-${chr4() + chr4() + chr4()}`;
+}
