18a5bf7e81281c303ee125a27c67bd509e934b11
Kirill Agalakov <agalakov@devexperts.com>
2016-12-06 12:17:53 +0300

    Session

---
+++ src/session/Session.js
@@ -0,0 +1,79 @@
+import {uuid} from '../string/string';
+import {DISPOSABLE} from '../function/disposable';
+import Emitter from '../emitter/Emitter';
+
+
+const id = uuid();
+const EVENT_KEY = '__SESSION_EVENT__';
+
+/**
+ * @enum
+ */
+export const E_SESSION = {
+	REQUEST: 'E_SESSION:REQUEST'
+};
+
+/**
+ * @typedef {{}} TRequestEvent
+ * @property {String} receiver - sid
+ * @property {*} request
+ */
+
+/**
+ * @emits {@link E_SESSION}
+ */
+@DISPOSABLE
+class Session extends Emitter {
+	/**
+	 * Current session id
+	 * @returns {String}
+	 */
+	get id() {
+		return id;
+	}
+
+	constructor() {
+		super();
+		window.addEventListener('storage', this._onStorage);
+		this._using([
+			window.removeEventListener('storage', this._onStorage)
+		]);
+	}
+
+	/**
+	 * Sends message to another session by id
+	 * @param {String} sid
+	 * @param {*} request
+	 */
+	send(sid, request) {
+		const data = JSON.stringify({
+			receiver: sid,
+			request
+		});
+		window.localStorage.setItem(EVENT_KEY, data);
+	}
+
+	/**
+	 * @param {Event} event
+	 * @private
+	 */
+	_onStorage = (event) => {
+		if (event.key === EVENT_KEY) {
+			try {
+				/**
+				 * @type {TRequestEvent}
+				 */
+				const value = JSON.parse(event.newValue);
+				if (value.receiver === id) {
+					this._emit(E_SESSION.REQUEST, value.request);
+				}
+			} catch (e) {}
+		}
+	}
+}
+
+export default typeof window !== 'undefined' ? new Session() : {
+	id,
+	send(sid, request) {
+	}
+};
