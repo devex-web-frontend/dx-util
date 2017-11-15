a0a78151a3c223a718d712165b0c6c47e9031d53
Kirill Agalakov <agalakov@devexperts.com>
2017-11-15 17:33:42 +0300

    add DOM contains

--- src/dom/dom.ts
+++ src/dom/dom.ts
@@ -5,8 +5,8 @@ import { capitalize } from '../string/string';
  */
 export function getBoundingClientRect(element: HTMLElement): ClientRect & { middle: number, center: number } {
 	const rect = element.getBoundingClientRect();
-	const {top, left, right, bottom} = rect;
-	let {width, height} = rect;
+	const { top, left, right, bottom } = rect;
+	let { width, height } = rect;
 
 	width = width || element.offsetWidth;
 	height = height || element.offsetHeight;
@@ -59,4 +59,42 @@ export function setVendorStyle(element: HTMLElement, property: string, value: an
  */
 function isPlainObject(object: object): boolean {
 	return object && typeof object === 'object' && !Array.isArray(object);
+}
+
+/**
+ * Checkes if DOM can be access in current environment
+ */
+export function canUseDOM(): boolean {
+	return Boolean(typeof window !== 'undefined' && window.document && window.document.createElement);
+}
+
+/**
+ * Checks if context contains node in the DOM tree
+ * @see https://github.com/react-bootstrap/dom-helpers/blob/master/src/query/contains.js
+ */
+export const contains: (context: Node, node: Node) => boolean =
+	!canUseDOM() ? fallback : (context, node) => {
+		if (context.contains) {
+			return context.contains(node);
+		} else if (context.compareDocumentPosition) {
+			return context === node || !!(context.compareDocumentPosition(node) & 16); //tslint:disable-line no-bitwise
+		} else {
+			return fallback(context, node);
+		}
+	};
+
+/**
+ * @see  https://github.com/react-bootstrap/dom-helpers/blob/master/src/query/contains.js
+ */
+function fallback(context: Node, node: Node): boolean {
+	if (node) {
+		let current: Node | null = node;
+		do {
+			if (current === context) {
+				return true;
+			}
+		} while ((current = current.parentNode)); //tslint:disable-line no-conditional-assignment
+	}
+
+	return false;
 }
